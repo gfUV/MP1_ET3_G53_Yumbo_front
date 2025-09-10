5fa2e61e677db5e57500530059c7aa2a1b7f5de4
alexandra190220 <maria.alexandra.morales@correounivalle.edu.co>
2025-09-09 22:06:03 -0500

    try 2 vercel

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+window.location.href = '/src/sign_in.html';
 
 document.querySelector('#app').innerHTML = `
   <div>
