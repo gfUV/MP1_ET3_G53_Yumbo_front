541fb1b64d8ce0701fa4194091dc50708c16aae1
alexandra190220 <maria.alexandra.morales@correounivalle.edu.co>
2025-09-12 11:16:47 -0500

    prueba vercel 1

--- api/routes/session.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const SessionController = require("../controllers/SessionController");
-
-// Endpoint de login
-router.post("/login", (req, res) => SessionController.login(req, res));
-
-module.exports = router;
