dbadd55a569901145411d13872d797d200f81153
gfUV <german.franco@correounivalle.edu.co>
2025-09-17 23:42:31 -0500

    Added URL .../userId

--- src/profile.js
+++ src/profile.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
     // 2. Llamar al backend para traer todos los usuarios
-    const response = await fetch("https://mp1-et3-g53-yumbo-back.onrender.com/api/v1/users");
+    const response = await fetch("https://mp1-et3-g53-yumbo-back.onrender.com/api/v1/users/userId");
     if (!response.ok) throw new Error("Error al cargar datos de usuarios");
 
     const users = await response.json();
