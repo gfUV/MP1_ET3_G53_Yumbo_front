807511fbcb341db57131fecc48002b79fda2714b
gfUV <german.franco@correounivalle.edu.co>
2025-09-18 19:52:23 -0500

    Modified adress conection reset_password.html with reset_password.js

--- public/resetPass_confirm.js
+++ public/resetPass_confirm.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const newPassword = form.password.value;
 
     try {
-      const res = await fetch("http://localhost:3000/api/v1/auth/reset-password/confirm", {
+      const res = await fetch("https://mp1-et3-g53-yumbo-back.onrender.com/api/v1/auth/reset-password/confirm", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ token, newPassword }),
